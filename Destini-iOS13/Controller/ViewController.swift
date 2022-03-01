9609b086cea5964f828b5b4348ded15614b11cdc
Oscar Pacheco <Oscarpa9188@gmail.com>
2022-02-28 19:47:07 -0600

    Added Constraints

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -9,10 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var backgroundImage: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        
     }
 
 
